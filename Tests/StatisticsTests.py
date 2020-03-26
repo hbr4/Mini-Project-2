85e94a471e21f830d40c54942d52dbe919476a6c
Hiren Rana <hbr4@njit.edu>
2020-03-26 19:14:54 -0400

    Updated Statistics Tests to clean up code

--- Tests/StatisticsTests.py
+++ Tests/StatisticsTests.py
@@ -65,7 +65,21 @@ class MyTestCase(unittest.TestCase):
     def test_confidence_interval(self):
         for row in self.test_answer:
             pprint(row['confidence'])
-        self.assertEqual(self.statistics.confidence(self.column1), float(row['confidence']))
+        self.assertEqual(self.statistics.confidence_up(self.column1), float(row['confidence']))
+        self.assertEqual(self.statistics.result, float(row['confidence']))
+
+    def test_quartile_statistics(self):
+        for row in self.test_answer:
+            pprint(row['quartile1'])
+            pprint(row['quartile2'])
+        self.assertEqual(self.statistics.quartiles(self.column1), float(row['quartile1']))
+        self.assertEqual(self.statistics.quartiles(self.column1), float(row['quartile2']))
+
+    def test_skewness_statistics(self):
+        for row in self.test_answer:
+            pprint(row['skewness'])
+        self.assertEqual(self.statistics.skewness(self.column1), float(row['skewness']))
+        self.assertEqual(self.statistics.result, float(row['skewness']))
 
 
 if __name__ == '__main__':
